b2868f1a8dc694d6fbe7ba30977a298faa68dc31
Pier Janssen <pier@threesixty.nl>
2011-11-19 23:11:24 +0100

    Changed bundle.bat to work with GIT repo; added README

--- bundle.bat
+++ bundle.bat
@@ -1,15 +1,18 @@
 @ECHO OFF
 
-CALL :GetZip zip
-dir "%zip%" > nul || goto :PathError "Could not find 7z.exe."
-
-
 SET dir=%~dp0
 SET Configuration=Release
-SET sourceDir=%dir%maxscript\
 SET targetDir=%dir%mzp\
 SET output=%dir%outliner.mzp
 
+
+ECHO Checking bundle prerequisites...
+CALL :GetZip zip
+dir "%zip%" > nul || goto :ZipNotFoundError
+ECHO Found 7Zip.
+
+
+ECHO.
 ECHO Removing old target files and directory...
 
 ::Remove target dir if it exists
@@ -21,23 +24,33 @@ IF EXIST %targetDir% (
 ::Remove output file if it exists
 IF EXIST %output% ( del /Q /S %output% || goto :error )
 
-::Perform SVN export of source dir
+
+
+::Copy the maxscript dir to a temporary directory.
 ECHO.
-ECHO Exporting maxscript from svn...
-svn export %sourceDir% %targetDir% || goto :error
+ECHO Creating maxscript to temporary mzp directory...
+xcopy %dir%maxscript %targetDir% /e /q || goto :error
+
+
 
 ::Copy Outliner.dll from dotnet to maxscript
 ECHO.
 ECHO Copying Outliner.dll to bundle...
-copy %dir%dotnet\bin\%Configuration%\Outliner.dll %targetDir%script\Outliner.dll || goto :error
+copy %dir%dotnet\bin\%Configuration%\Outliner.dll %targetDir%script\Outliner.dll || goto :OutlinerDllError
 
-::Create output from target dir
+
+
+::Create package from target dir
 ECHO.
 ECHO Packing mzp...
 "%zip%" a -tzip %output% %targetDir%* || goto :error
 
+
+
 ::Remove target dir
 rmdir /Q /S %targetDir% || goto :error
+
+
 
 ECHO Done.
 goto :eof
@@ -52,8 +65,12 @@ FOR /F "tokens=2*" %%A IN ('REG QUERY "%KEY_NAME%" /v %KEY_VALUE% 2^>nul') DO (
 )
 goto :eof
 
-:PathError
-ECHO %~1
+:ZipNotFoundError
+ECHO Could not find 7z.exe.
+goto :error
+
+:OutlinerDllError
+ECHO You may have to build the .NET library first using buildandbundle.bat
 goto :error
 
 :error
